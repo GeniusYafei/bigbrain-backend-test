181bd43037d901d53597925ec60f33c836c3670e
Superstar.W <yafei@YafeiMacBook-Air.local>
2025-04-24 16:46:48 +1000

    fix: add CORS headers to support frontend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 // Backend-test/api/index.js
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-import { AccessError, InputError } from '../src/error.js';
+import { AccessError, InputError } from "../src/error.js";
 
 import {
   assertOwnsGame,
@@ -23,14 +23,20 @@ import {
   sessionResults,
   sessionStatus,
   submitAnswers,
-  updateGamesFromAdmin
-} from '../src/service.js';
+  updateGamesFromAdmin,
+} from "../src/service.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // https://bigbrain-frontend-olive.vercel.app
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.json({ limit: "100mb" }));
 
 // Error handling wrapper
 const catchErrors = (fn) => async (req, res) => {
@@ -44,116 +50,170 @@ const catchErrors = (fn) => async (req, res) => {
       res.status(403).send({ error: err.message });
     } else {
       console.log(err);
-      res.status(500).send({ error: 'A system error occurred' });
+      res.status(500).send({ error: "A system error occurred" });
     }
   }
 };
 
 // Auth wrapper
 const authed = (fn) => async (req, res) => {
-  const email = getEmailFromAuthorization(req.header('Authorization'));
+  const email = getEmailFromAuthorization(req.header("Authorization"));
   await fn(req, res, email);
 };
 
 /***************************************************************
                        Auth Functions
 ***************************************************************/
-app.post('/admin/auth/login', catchErrors(async (req, res) => {
-  const { email, password } = req.body;
-  const token = await login(email, password);
-  return res.json({ token });
-}));
+app.post(
+  "/admin/auth/login",
+  catchErrors(async (req, res) => {
+    const { email, password } = req.body;
+    const token = await login(email, password);
+    return res.json({ token });
+  })
+);
 
-app.post('/admin/auth/register', catchErrors(async (req, res) => {
-  const { email, password, name } = req.body;
-  const token = await register(email, password, name);
-  return res.json({ token });
-}));
+app.post(
+  "/admin/auth/register",
+  catchErrors(async (req, res) => {
+    const { email, password, name } = req.body;
+    const token = await register(email, password, name);
+    return res.json({ token });
+  })
+);
 
-app.post('/admin/auth/logout', catchErrors(authed(async (req, res, email) => {
-  await logout(email);
-  return res.json({});
-})));
+app.post(
+  "/admin/auth/logout",
+  catchErrors(
+    authed(async (req, res, email) => {
+      await logout(email);
+      return res.json({});
+    })
+  )
+);
 
 /***************************************************************
                       Game Functions
 ***************************************************************/
-app.get('/admin/games', catchErrors(authed(async (req, res, email) => {
-  const games = await getGamesFromAdmin(email);
-  return res.json({ games });
-})));
+app.get(
+  "/admin/games",
+  catchErrors(
+    authed(async (req, res, email) => {
+      const games = await getGamesFromAdmin(email);
+      return res.json({ games });
+    })
+  )
+);
 
-app.put('/admin/games', catchErrors(authed(async (req, res, email) => {
-  if (!req.body || !req.body.games) {
-    throw new InputError("Request body must contain a 'games' field");
-  }
-  const { games } = req.body;
-  if (!Array.isArray(games)) {
-    throw new InputError("Games must be an array");
-  }
-  await updateGamesFromAdmin({ gamesArrayFromRequest: games, email });
-  return res.status(200).send({});
-})));
+app.put(
+  "/admin/games",
+  catchErrors(
+    authed(async (req, res, email) => {
+      if (!req.body || !req.body.games) {
+        throw new InputError("Request body must contain a 'games' field");
+      }
+      const { games } = req.body;
+      if (!Array.isArray(games)) {
+        throw new InputError("Games must be an array");
+      }
+      await updateGamesFromAdmin({ gamesArrayFromRequest: games, email });
+      return res.status(200).send({});
+    })
+  )
+);
 
-app.post('/admin/game/:gameid/mutate', catchErrors(authed(async (req, res, email) => {
-  const { gameid } = req.params;
-  await assertOwnsGame(email, gameid);
-  const { mutationType } = req.body;
-  const data = await mutateGame({
-    gameId: gameid,
-    mutationType
-  });
-  return res.status(200).send({ data });
-})));
+app.post(
+  "/admin/game/:gameid/mutate",
+  catchErrors(
+    authed(async (req, res, email) => {
+      const { gameid } = req.params;
+      await assertOwnsGame(email, gameid);
+      const { mutationType } = req.body;
+      const data = await mutateGame({
+        gameId: gameid,
+        mutationType,
+      });
+      return res.status(200).send({ data });
+    })
+  )
+);
 
-app.get('/admin/session/:sessionid/status', catchErrors(authed(async (req, res, email) => {
-  const { sessionid } = req.params;
-  await assertOwnsSession(email, sessionid);
-  return res.status(200).json({ results: await sessionStatus(sessionid) });
-})));
+app.get(
+  "/admin/session/:sessionid/status",
+  catchErrors(
+    authed(async (req, res, email) => {
+      const { sessionid } = req.params;
+      await assertOwnsSession(email, sessionid);
+      return res.status(200).json({ results: await sessionStatus(sessionid) });
+    })
+  )
+);
 
-app.get('/admin/session/:sessionid/results', catchErrors(authed(async (req, res, email) => {
-  const { sessionid } = req.params;
-  await assertOwnsSession(email, sessionid);
-  return res.status(200).json({ results: await sessionResults(sessionid) });
-})));
+app.get(
+  "/admin/session/:sessionid/results",
+  catchErrors(
+    authed(async (req, res, email) => {
+      const { sessionid } = req.params;
+      await assertOwnsSession(email, sessionid);
+      return res.status(200).json({ results: await sessionResults(sessionid) });
+    })
+  )
+);
 
 /***************************************************************
                       Play Functions
 ***************************************************************/
-app.post('/play/join/:sessionid', catchErrors(async (req, res) => {
-  const { sessionid } = req.params;
-  const { name } = req.body;
-  const playerId = await playerJoin(name, sessionid);
-  return res.status(200).send({ playerId });
-}));
+app.post(
+  "/play/join/:sessionid",
+  catchErrors(async (req, res) => {
+    const { sessionid } = req.params;
+    const { name } = req.body;
+    const playerId = await playerJoin(name, sessionid);
+    return res.status(200).send({ playerId });
+  })
+);
 
-app.get('/play/:playerid/status', catchErrors(async (req, res) => {
-  const { playerid } = req.params;
-  return res.status(200).send({ started: await hasStarted(playerid) });
-}));
+app.get(
+  "/play/:playerid/status",
+  catchErrors(async (req, res) => {
+    const { playerid } = req.params;
+    return res.status(200).send({ started: await hasStarted(playerid) });
+  })
+);
 
-app.get('/play/:playerid/question', catchErrors(async (req, res) => {
-  const { playerid } = req.params;
-  return res.status(200).send({ question: await getQuestion(playerid) });
-}));
+app.get(
+  "/play/:playerid/question",
+  catchErrors(async (req, res) => {
+    const { playerid } = req.params;
+    return res.status(200).send({ question: await getQuestion(playerid) });
+  })
+);
 
-app.get('/play/:playerid/answer', catchErrors(async (req, res) => {
-  const { playerid } = req.params;
-  return res.status(200).send({ answers: await getAnswers(playerid) });
-}));
+app.get(
+  "/play/:playerid/answer",
+  catchErrors(async (req, res) => {
+    const { playerid } = req.params;
+    return res.status(200).send({ answers: await getAnswers(playerid) });
+  })
+);
 
-app.put('/play/:playerid/answer', catchErrors(async (req, res) => {
-  const { playerid } = req.params;
-  const { answers } = req.body;
-  await submitAnswers(playerid, answers);
-  return res.status(200).send({});
-}));
+app.put(
+  "/play/:playerid/answer",
+  catchErrors(async (req, res) => {
+    const { playerid } = req.params;
+    const { answers } = req.body;
+    await submitAnswers(playerid, answers);
+    return res.status(200).send({});
+  })
+);
 
-app.get('/play/:playerid/results', catchErrors(async (req, res) => {
-  const { playerid } = req.params;
-  return res.status(200).send(await getResults(playerid));
-}));
+app.get(
+  "/play/:playerid/results",
+  catchErrors(async (req, res) => {
+    const { playerid } = req.params;
+    return res.status(200).send(await getResults(playerid));
+  })
+);
 
 /***************************************************************
                Vercel Serverless API Entry Point
