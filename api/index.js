5937c0d2ea5ad988348ebea744cc9085bc98925a
Superstar.W <yafei@YafeiMacBook-Air.local>
2025-04-24 16:56:52 +1000

    fix: correct CORS origin URL (removed trailing slash)

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://bigbrain-frontend-olive.vercel.app/", // https://bigbrain-frontend-olive.vercel.app
+    origin: "https://bigbrain-frontend-olive.vercel.app", // https://bigbrain-frontend-olive.vercel.app
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
